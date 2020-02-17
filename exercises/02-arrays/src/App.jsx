b3db0d1bc29c80459b3bb61e067d102d5dcdf01a
Brian Ha <brianha32@gmail.com>
2020-02-16 22:37:15 -0500

    Continuously getting errors when running the app after many code edits.

--- exercises/02-arrays/src/App.jsx
+++ exercises/02-arrays/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 // import something
 import "./App.css";
+// importing the array of images from image directory
+import {images} from "./images/images";
 
 function App() {
-  return <div className="App">{/* complete me */}</div>;
+  return (<div className="App">{
+    images.map((image, i) => (
+      <img 
+      src={image.src}
+      key={i} 
+      />
+    ))
+  }</div>
+  );
 }
 
 export default App;
