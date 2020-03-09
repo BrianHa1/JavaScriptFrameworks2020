f341ca0509f4d9665f5ca7bc91a300d6eaf7935b
Brian Ha <brianha32@gmail.com>
2020-03-09 18:46:17 -0400

    More files committed.

--- exercises/08b-context-api/src/components/App/App.jsx
+++ exercises/08b-context-api/src/components/App/App.jsx
@@ -1,10 +1,11 @@
 // Import what you need from React here
-import React from "react";
+import React, {useState} from "react";
 // You will need to import the "TranslatorContext" from src/contexts/TranslatorContext.js
 import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import CreateAccount from "../CreateAccount/CreateAccount";
 import Footer from "../Footer/Footer";
+import TranslatorContext from "../../contexts/TranslatorContext";
 
 function App() {
   /**
@@ -12,16 +13,19 @@ function App() {
    * @see exercises/08a-context-api/src/App.jsx
    * @see solutions/08a-context-api/App.solutions.jsx
    */
+  const [language, setLanguage] = useState("en");
   return (
-    <div className="App d-flex flex-column">
-      <NavBar />
-      <div className="container pt-4 pb-4">
-        <CreateAccount />
+    <TranslatorContext.Provider value={[language, setLanguage]}>
+      <div className="App d-flex flex-column">
+        <NavBar />
+        <div className="container pt-4 pb-4">
+          <CreateAccount />
+        </div>
+        <div className="mt-auto">
+          <Footer />
+        </div>
       </div>
-      <div className="mt-auto">
-        <Footer />
-      </div>
-    </div>
+    </TranslatorContext.Provider>
   );
 }
 
