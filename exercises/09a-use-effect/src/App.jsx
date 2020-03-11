3c557f0208f2ad848df0fc9221795e13ec8307cf
Brian Ha <brianha32@gmail.com>
2020-03-10 21:31:43 -0400

    More files.

--- exercises/09a-use-effect/src/App.jsx
+++ exercises/09a-use-effect/src/App.jsx
@@ -1,17 +1,30 @@
 // Import useEffect here
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 // import Axios (or use Fetch)
+import axios from "axios";
 
 function App() {
   /**
    * Set up your state
    */
-
+  const [quote, setQuote] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
   /**
    * Make an AJAX call with the useEffect hook
    */
-
+  useEffect(async () => {
+    try{
+      let response = await axios.get("https://ron-swanson-quotes.herokuapp.com/v2/quotes");
+      setQuote(response.data[0]);
+    }
+    catch (e){
+      console.error(e);
+      setHasError(true);
+    }
+    setIsLoading(false);
+  }, []);
   return (
     <body class="bg-warning text-center">
     <img
@@ -22,13 +35,23 @@ function App() {
       class="mt-4"
     />
     <div class="container">
-      {/* Display a loading message */}
-      {/* Display an error message if the API fails */}
+      {
+        isLoading && <div>Loading...</div>
+      }
+      {
+        hasError 
+        && 
+        <div className="error bg-white text-secondary border border-secondary mb-4">
+          Oh no! An unexpected error has occurred while the request was being made.
+        </div>
+      }
       <blockquote
         id="quote"
         class="blockquote bg-dark text-white border-0 mb-4"
       >
-        {/* Complete me */}
+        {
+          quote
+        }
       </blockquote>
     </div>
   </body>
