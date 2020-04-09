483c753fd29963dc5a38ec9b91bb2eb89041b1d7
Brian Ha <brianha32@gmail.com>
2020-04-08 20:51:19 -0400

    Exercise 13 completed.

--- exercises/13-class-ajax/src/App.js
+++ exercises/13-class-ajax/src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
 /** Convert me into a class component  */
+class App extends Component {
+  state = {
+    quote: "",
+    isLoading: true,
+    hasError: false
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ron Swanson</h1>
-    </div>
-  );
+  componentDidMount() {
+    axios("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
+    .then(response => this.setState({quote: response.data}))
+    .catch(err => this.setState({hasError: true}))
+    .then(() => this.setState({isLoading: false}));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ron Swanson</h1>
+        <p>{this.state.quote}</p>
+        {this.state.isLoading && <p>Loading...</p>}
+        {this.state.hasError && <p>There is an error</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
